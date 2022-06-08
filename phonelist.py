857f1289a37e0b255e9f682b23303c6baa988b0e
aram özer <aramozer@gmail.com>
2022-06-08 14:40:26 +0200

    #2.6.2 §7. - upper case commands

--- phonelist.py
+++ phonelist.py
@@ -38,7 +38,7 @@ list - list all phone numbers
 quit - quit the program''')
 
 while True: ## REPL - Read Execute Program Loop
-    cmd = input("Command: ")
+    cmd = input("Command: ").upper()
     if cmd == "LIST":
         print(read_phonelist(conn))
     elif cmd == "ADD":
